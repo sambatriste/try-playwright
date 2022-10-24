7d55121c3ef40d3aea8c6921a17a060e2f023b25
tyookk <mahaweilu@gmail.com>
2022-10-24 16:04:46 +0900

    feature:トップページのテストケース追加

--- src/test/java/com/example/TopPageTest.java
+++ src/test/java/com/example/TopPageTest.java
@@ -76,6 +76,69 @@ public class TopPageTest {
 
         topPage.clickLatestArticleReadMore();
         assertThat(cards).hasCount(16);
+    }
 
+    @Test
+    @DisplayName("最新記事の「短く表示する」をクリックすると記事が追加で表示されること")
+    void displayShortly(Page page) {
+        TopPage topPage = new TopPage(page);
+        topPage.navigate();
+        topPage.clickDisplayLatestArticlesShortly();
+    }
+
+    @Test
+    @DisplayName("最新記事リストの記事にメインカテゴリ、タイトル、抜粋内容、タグ、著者アイコン、著者名、日付、いいね数が表示されていること")
+    void blogDetailsArelVisible(Page page) {
+        TopPage topPage = new TopPage(page);
+        topPage.navigate();
+        // メインカテゴリ
+        assertThat(page.locator("li.o-card > .o-card__content > a >.o-card__head > .o-card__category").first()).isVisible();
+        // タイトル
+        assertThat(page.locator("li.o-card > .o-card__content > a >.o-card__title").first()).isVisible();
+        // 抜粋内容
+        assertThat(page.locator("li.o-card > .o-card__content > a >.o-card__text").first()).isVisible();
+        // タグ
+        assertThat(page.locator("li.o-card > .o-card__content > .o-card__tag").first()).isVisible();
+        // 著者アイコン
+        assertThat(page.locator("li.o-card > .o-card__content > .o-card__user > .image").first()).isVisible();
+        // 著者名
+        assertThat(page.locator("li.o-card > .o-card__content > .o-card__user > .info > h5").first()).isVisible();
+        // 日付
+        assertThat(page.locator("li.o-card > .o-card__content > .o-card__user > .info > .bottom > time").first()).isVisible();
+        // いいね数
+        assertThat(page.locator("li.o-card > .o-card__content > .o-card__user > .info > .bottom > .like").first()).isVisible();
+    }
+
+    @Test
+    @DisplayName("リンク押下で該当記事に繊維できる")
+    void clickBlogLink(Page page) {
+        TopPage topPage = new TopPage(page);
+        topPage.navigate();
+        Locator blogLink = page.locator("li.o-card > .o-card__content > a").first();
+        String destinationUrl = blogLink.getAttribute("href");
+        blogLink.click();
+        assertThat(page).hasURL(destinationUrl);
+    }
+
+    @Test
+    @DisplayName("カテゴリリンクをホバーしてカテゴリ一覧が表示されること")
+    void hoverCategoryLink(Page page) {
+        TopPage topPage = new TopPage(page);
+        topPage.navigate();
+        topPage.header.hoverCategoryLink();
+    }
+
+    @Test
+    @DisplayName("カテゴリリンクをホバーして正しい内容のカテゴリ一覧が表示されること")
+    void categoryListIsCorrect(Page page) {
+        TopPage topPage = new TopPage(page);
+        topPage.navigate();
+
+        Locator categoryMenuLinks = page.locator(".sub-menu.js-menu > ul > li > a");
+        String[] expectedCategoryNames = new String[] {"モバイルアプリケーション開発", "UX/UIデザイン", "Webアプリケーション開発", "先進技術研究", "新規事業開発", "その他のカテゴリ"};
+
+        assertThat(categoryMenuLinks).hasCount(6);
+
+        assertThat(categoryMenuLinks).containsText(expectedCategoryNames);
     }
 }
