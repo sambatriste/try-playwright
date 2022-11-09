1cfb22bfe53c2e0433395ff1a60501ac8ee27aad
tyookk <mahaweilu@gmail.com>
2022-11-09 13:25:40 +0900

    style:リファクタリング

--- src/test/java/com/example/BlogDetailPageTest.java
+++ src/test/java/com/example/BlogDetailPageTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @ExtendWith(PlaywrightExtension.class)
