ba626ad64b2c68451829dacaa0389cbf15659a5d
mahaweilu <37402719+mahaweilu@users.noreply.github.com>
2022-11-15 10:37:52 +0900

    fix:スペースを修正。トップページ見出しタグテストを追加 (#6)
    * fix:スペースを修正。トップページ見出しタグテストを追加
    
    * style:リファクタリング
    
    * style:リファクタリング
    
    * style:リファクタリング

--- src/test/java/com/example/TopPageTest.java
+++ src/test/java/com/example/TopPageTest.java
@@ -276,4 +276,17 @@ public class TopPageTest {
         String actualMetaDescription = metaDescription.first().getAttribute("content");
         assertEquals(META_DESCRIPTION, actualMetaDescription);
     }
+
+    @Test
+    @DisplayName("h2タグが正しい内容であること")
+    void hasCorrectH2tag(Page page) {
+        TopPage topPage = new TopPage(page);
+        topPage.navigate();
+        Locator h2Tags = page.locator("h2");
+        // 空のh2内容はJetpackが生成したもの（ほんとはJetpackの設定を見なし不要なh2を出入しないようにするが良いが影響範囲が判定されていないため暫定このまま）
+        // https://ja.jetpack.com/support/carousel/
+        String[] h2TagTexts = new String[] {"Fintanとは", "キーワードでさがす", "おすすめ記事", "最新記事", "人気記事", "お知らせ", "", ""};
+        assertThat(h2Tags).hasCount(h2TagTexts.length);
+        assertThat(h2Tags).containsText(h2TagTexts);
+    }
 }
