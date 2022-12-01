6612519ab5d9f4033702024e65b668509d8647ec
saito.takuma.814 <saito.takuma.814@tis.co.jp>
2022-12-01 15:11:30 +0900

    カテゴリ件数のテストを追加

--- src/test/java/com/example/CategoryListPageTest.java
+++ src/test/java/com/example/CategoryListPageTest.java
@@ -9,12 +9,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 @ExtendWith(PlaywrightExtension.class)
 public class CategoryListPageTest {
-    private static final String[] categoryNamesTestEnv = new String[] {
+    private static final String[] categoryNamesTestEnv = new String[]{
         "その他", "アジャイル・スクラム", "エンジニア育成・学習",
         "セキュリティ・暗号化", "ソフトウェアテスティング", "ブロックチェーン",
         "モバイルアプリケーション開発", "環境構築・ログ・CI/ CD", "量子コンピュータ",
@@ -23,7 +28,7 @@ public class CategoryListPageTest {
         "先進技術研究", "新規事業開発", "活動発信・イベントレポート"
     };
 
-    private static final String[] categoryNamesProductionEnv = new String[] {
+    private static final String[] categoryNamesProductionEnv = new String[]{
         "Webアプリケーション開発", "モバイルアプリケーション開発", "新規事業開発",
         "先進技術研究", "UX/UIデザイン", "XR",
         "ブロックチェーン", "Nablarch", "Lerna",
@@ -78,5 +83,36 @@ public class CategoryListPageTest {
         Locator h1Tag = page.locator("h1");
         assertThat(h1Tag).hasCount(1);
         assertThat(h1Tag).hasText("Category");
+    }
+
+    @Test
+    @DisplayName("カテゴリの件数が正しいこと")
+    void checkCategoryPostCount(Page page) {
+        CategoryListPage categoryListPage = new CategoryListPage(page);
+        categoryListPage.navigate();
+
+        Locator categories = categoryListPage.getCategoryNameLinks();
+        for (int i = 0; i < categories.count(); i++) {
+            Locator category = categories.nth(i);
+
+            // カテゴリの合計記事数を取得する
+            String content = category.textContent();
+            // ()の中の数字を抽出
+            Matcher matcher = Pattern.compile("(?<=\\().*?(?=\\))").matcher(content);
+            if (!matcher.find()) {
+                fail("Not match found.");
+            }
+            int sumCount = Integer.parseInt(matcher.group(0));
+
+            // カテゴリトップページへ遷移し、表示されている記事の数を取得する
+            String categoryTopUrl = category.getAttribute("href");
+            page.navigate(categoryTopUrl);
+            int postCount = page.locator("ul.o-card-list > li.o-card").count();
+
+            assertEquals(sumCount, postCount);
+
+            // カテゴリ一覧ページへと戻る
+            categoryListPage.navigate();
+        }
     }
 }
